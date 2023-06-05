053f9711983cb2159d51af06d53f69c43aa34780
Scott Lozano <scottloz@protonmail.com>
2023-06-05 21:05:41 +0100

    updates seed to include model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ cars = []
 users = []
 10.times do
   users << User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: Faker::Internet.password)
-  cars << Car.create!(year: 2021, price: "$#{Faker::Commerce.price}", seats_number: count, availability: true, user_id: count)
+  cars << Car.create!(model: "Ford" ,year: 2021, price: "$#{Faker::Commerce.price}", seats_number: count, availability: true, user_id: count)
   bookings << Booking.create!(start_date: "2021-01-01", end_date: "2021-01-02", confirmed: true, car_id: count, user_id: count)
   count += 1
 end
