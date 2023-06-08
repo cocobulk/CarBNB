970a9b473c92f2d95e9f05bb3d98dcf167ee8464
Scott Lozano <scottloz@protonmail.com>
2023-06-08 23:29:34 +0100

    updates wallet functions and packages

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1
-    })
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      'process.env.NODE_DEBUG': JSON.stringify(process.env.NODE_DEBUG),
+    }),
   ]
 }
