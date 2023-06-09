508e7eaec20981e58131ead0bd6f68f1a8a41c4e
Scott Lozano <scottloz@protonmail.com>
2023-06-09 10:17:19 +0100

    saves changes

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: "production",
   devtool: "source-map",
   entry: {
-    application: "./app/javascript/application.js"
+    application: "/app/javascript/application.js"
   },
   output: {
     filename: "[name].js",
@@ -17,7 +17,6 @@ module.exports = {
       maxChunks: 1
     }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
       'process.env.NODE_DEBUG': JSON.stringify(process.env.NODE_DEBUG),
     }),
   ]
